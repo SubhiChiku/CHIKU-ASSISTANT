50491b48315c7cdd814939aa97a3cd5168882e06
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-02-13 02:33:31 +0530

    update

--- CHIKU/modules/userinfo.py
+++ CHIKU/modules/userinfo.py
@@ -285,7 +285,7 @@ def info(update: Update, context: CallbackContext):
         disaster_level_present = True
 
     if disaster_level_present:
-        text += ' \n[<a href="https://t.me/HEROKUFREECC">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ ɪs ᴅɪsᴀsᴛᴇʀ ʟᴇᴠᴇʟs.</a>]'.format(
+        text += ' \n[<a href="https://t.me/+3qCjioLlvxZhZDk1">ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴋɴᴏᴡ ᴡʜᴀᴛ ɪs ᴅɪsᴀsᴛᴇʀ ʟᴇᴠᴇʟs.</a>]'.format(
             bot.username
         )
 
@@ -323,10 +323,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url=f"https://t.me/HEROKUFREECC"
+                                "ʜᴇᴀʟᴛʜ", url=f"https://t.me/+3qCjioLlvxZhZDk1"
                             ),
                             InlineKeyboardButton(
-                                "Dɪsᴀsᴛᴇʀ", url="https://t.me/HEROKUFREECC"
+                                "Dɪsᴀsᴛᴇʀ", url="https://t.me/+3qCjioLlvxZhZDk1"
                             ),
                         ],
                         [
@@ -349,10 +349,10 @@ def info(update: Update, context: CallbackContext):
                     [
                         [
                             InlineKeyboardButton(
-                                "ʜᴇᴀʟᴛʜ", url="https://t.me/HEROKUFREECC"
+                                "ʜᴇᴀʟᴛʜ", url="https://t.me/+3qCjioLlvxZhZDk1"
                             ),
                             InlineKeyboardButton(
-                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/HEROKUFREECC"
+                                "ᴅɪsᴀsᴛᴇʀ", url="https://t.me/+3qCjioLlvxZhZDk1"
                             ),
                         ],
                         [
